253254bcfdce8bdb11085cdda268ac5d97969ace
Trung <hotrung1548@gmail.com>
2025-03-17 08:12:27 +0700

    [Thực hành] Truy vấn dữ liệu với CSDL Quản lý sinh viên

--- QuanLySinhVien.sql
+++ QuanLySinhVien.sql
@@ -67,3 +67,19 @@ VALUES (1, 'CF', 5, 1),
 VALUES (1, 1, 8, 1),
  (1, 2, 10, 2),
  (2, 1, 12, 1);
+ 
+ -- [Thực hành] Truy vấn dữ liệu với CSDL Quản lý sinh viên 
+-- Hiển thị danh sách tất cả các học viên
+ select * from student;
+-- Hiển thị danh sách các học viên đang theo học.
+select * from student where status = true;
+-- Hiển thị danh sách các môn học có thời gian học nhỏ hơn 10 giờ.
+select * from subject where credit < 10;
+ -- Hiển thị danh sách học viên lớp A1
+ select S.StudentId, S.StudentName, C.ClassName FROM Student S join Class C on S.ClassId = C.ClassID
+ where C.classname = 'A1';
+ -- Hiển thị điểm môn CF của các học viên
+ select S.StudentId, S.StudentName, Sub.subname,  M.Mark FROM Student S join Mark M on  S.StudentId = M.StudentId 
+ join subject Sub on M.subId = Sub.subId
+ where sub.subname = 'CF';
+ 
